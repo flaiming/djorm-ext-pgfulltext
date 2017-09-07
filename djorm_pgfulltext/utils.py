a41f35be5802a302b2c5b46b019de6b21b6c20bb
Vojtěch Oram <vojtech@oram.cz>
2017-09-07 12:56:07 +0200

    reinitialize connection

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -6,5 +6,8 @@ from django.utils.text import force_text
 
 def adapt(text):
     a = psycopg2.extensions.adapt(force_text(text))
+    if connection.connection is None:
+        # re-initialize connection
+        connection.cursor().close()
     a.prepare(connection.connection)
     return a
